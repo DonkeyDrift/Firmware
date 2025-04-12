042bd8cc3bfa2b4622782790d9691a6d98cda264
nowhere2020 <haobot@foxmail.com>
2025-04-12 22:47:04 +0800

    fix bugs

--- mus4/mus4.ino
+++ mus4/mus4.ino
@@ -1,74 +1,85 @@
 //=============================================================v1===2025-04-12
+// Note
+// 1. 固件程序下载速率为115200
+
 // #include <esp_now.h>
 // #include <WiFi.h>
 #include <Wire.h>
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
-#include <FastLED.h>
-#include <ESP32Servo.h>
+// #include <Adafruit_MPU6050.h>
+// #include <Adafruit_Sensor.h>
+// #include <FastLED.h>
+// #include <ESP32Servo.h>
 
-#define LED_PIN 9
+#define LED_PIN 5
 #define NUM_LEDS 1
 #define BRIGHTNESS 64
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
 
-CRGB leds[NUM_LEDS]; // Define the array of leds
+#define BUAD_RATE_0 115200
+#define RX_1_PIN 16
+#define TX_1_PIN 17
+#define BUAD_RATE_1 115200
+#define SDA_PIN 13
+#define SCL_PIN 14
+#define I2C_SPEED 400000L
+
+// CRGB leds[NUM_LEDS]; // Define the array of leds
 
 // 新增全局变量
 bool toggleActive = false;
-CRGB toggleColor1, toggleColor2;
+// CRGB toggleColor1, toggleColor2;
 unsigned long toggleTime = 0;
 unsigned long toggleInterval = 250; // LED切换间隔为250ms
 
 // 修改setLEDColor函数
-void setLEDColor(CRGB targetColor)
-{
-    if (toggleActive)
-    {
-        toggleActive = false; // 关闭切换模式
-        toggleTime = 0;       // 重置切换时间
-    }
-    if (leds[0] != targetColor)
-    {
-        leds[0] = targetColor;
-        FastLED.show(); // 不一致时才更新显示
-    }
-}
+// void setLEDColor(CRGB targetColor)
+// {
+//     if (toggleActive)
+//     {
+//         toggleActive = false; // 关闭切换模式
+//         toggleTime = 0;       // 重置切换时间
+//     }
+//     if (leds[0] != targetColor)
+//     {
+//         leds[0] = targetColor;
+//         // FastLED.show(); // 不一致时才更新显示
+//     }
+// }
 
 // 新增setLEDToggle函数
-void setLEDToggle(CRGB color1, CRGB color2)
-{
-    toggleColor1 = color1;
-    toggleColor2 = color2;
-    toggleActive = true;
-    toggleTime = 0; // 确保首次切换立即执行
-}
+// void setLEDToggle(CRGB color1, CRGB color2)
+// {
+//     toggleColor1 = color1;
+//     toggleColor2 = color2;
+//     toggleActive = true;
+//     toggleTime = 0; // 确保首次切换立即执行
+// }
 
-void scanLEDToggle()
-{
-    if (toggleActive && (millis() >= toggleTime))
-    {
-        CRGB currentColor = leds[0];
-        CRGB nextColor = (currentColor == toggleColor1) ? toggleColor2 : toggleColor1;
-        leds[0] = nextColor;
-        FastLED.show();
-        toggleTime = millis() + toggleInterval;
-    }
-}
+// void scanLEDToggle()
+// {
+//     if (toggleActive && (millis() >= toggleTime))
+//     {
+//         CRGB currentColor = leds[0];
+//         CRGB nextColor = (currentColor == toggleColor1) ? toggleColor2 : toggleColor1;
+//         leds[0] = nextColor;
+//         // FastLED.show();
+//         toggleTime = millis() + toggleInterval;
+//     }
+// }
 
-Adafruit_MPU6050 mpu; // Create an MPU6050 object
-ESP32PWM pwm;         // Create a PWM object
+// Adafruit_MPU6050 mpu; // Create an MPU6050 object
+// ESP32PWM pwm; // Create a PWM object
 
 // #define DEBUG // Uncomment to enable debugging output
 
-#define CH1_PIN 6  // 接收机pwm输入CH1通道为，GPIO4
-#define CH2_PIN 7  // 接收机pwm输入CH2通道为，GPIO5
-#define CH3_PIN 8  // 接收机pwm输入CH3通道为，GPIO6
-#define CH4_PIN 10 // 接收机pwm输入CH4通道为，
+#define CH1_PIN 27 // 接收机pwm输入CH1通道
+#define CH2_PIN 26 // 接收机pwm输入CH2通道
+#define CH3_PIN 35 // 接收机pwm输入CH3通道
+#define CH4_PIN 34 // 接收机pwm输入CH4通道
 
-#define STEERING_PIN 4 // PIN of Servo
-#define THROTTLE_PIN 5 // PIN of ESC
+#define STEERING_PIN 32 // PIN of Servo
+#define THROTTLE_PIN 33 // PIN of ESC
 
 #define CH_STEERING 0 // index of pwm_value[]
 #define CH_THROTTLE 1 // index of pwm_value[]
@@ -82,7 +93,7 @@ ESP32PWM pwm;         // Create a PWM object
 volatile int pwm_value[4] = {0, 0, 0, 0};           // value of CH1, CH2, CH3, CH4
 volatile unsigned long rise_time[4] = {0, 0, 0, 0}; // time of rising edge of CH1, CH2, CH3, CH4
 
-const int Channels[4] = {6, 7, 8, 10};
+const int Channels[4] = {CH1_PIN, CH2_PIN, CH3_PIN, CH4_PIN};
 
 void IRAM_ATTR handle_interrupt(int channel)
 { // interrupt handler
@@ -245,128 +256,128 @@ void read_ina219()
     Serial.printf("%d:%f V\r\n", 2, I2CReadValue(0x41, 2) / 2 / 1000.0);
 }
 
-void read_mpu6050()
-{
-    /* Get new sensor events with the readings */
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
+// void read_mpu6050()
+// {
+//     /* Get new sensor events with the readings */
+//     sensors_event_t a, g, temp;
+//     mpu.getEvent(&a, &g, &temp);
 
-    /* Print out the values */
-    Serial.print("Acceleration X: ");
-    Serial.print(a.acceleration.x);
-    Serial.print(", Y: ");
-    Serial.print(a.acceleration.y);
-    Serial.print(", Z: ");
-    Serial.print(a.acceleration.z);
-    Serial.println(" m/s^2");
+//     /* Print out the values */
+//     Serial.print("Acceleration X: ");
+//     Serial.print(a.acceleration.x);
+//     Serial.print(", Y: ");
+//     Serial.print(a.acceleration.y);
+//     Serial.print(", Z: ");
+//     Serial.print(a.acceleration.z);
+//     Serial.println(" m/s^2");
 
-    Serial.print("Rotation X: ");
-    Serial.print(g.gyro.x);
-    Serial.print(", Y: ");
-    Serial.print(g.gyro.y);
-    Serial.print(", Z: ");
-    Serial.print(g.gyro.z);
-    Serial.println(" rad/s");
+//     Serial.print("Rotation X: ");
+//     Serial.print(g.gyro.x);
+//     Serial.print(", Y: ");
+//     Serial.print(g.gyro.y);
+//     Serial.print(", Z: ");
+//     Serial.print(g.gyro.z);
+//     Serial.println(" rad/s");
 
-    Serial.print("Temperature: ");
-    Serial.print(temp.temperature);
-    Serial.println(" degC");
+//     Serial.print("Temperature: ");
+//     Serial.print(temp.temperature);
+//     Serial.println(" degC");
 
-    Serial.println("");
-}
+//     Serial.println("");
+// }
 
-void setup_mpu6050()
-{
-    // Try to initialize!
-    if (!mpu.begin())
-    {
-        Serial.println("Failed to find MPU6050 chip");
-        while (1)
-        {
-            delay(10);
-        }
-    }
-    Serial.println("MPU6050 Found!");
+// void setup_mpu6050()
+// {
+//     // Try to initialize!
+//     if (!mpu.begin())
+//     {
+//         Serial.println("Failed to find MPU6050 chip");
+//         while (1)
+//         {
+//             delay(10);
+//         }
+//     }
+//     Serial.println("MPU6050 Found!");
 
-    // set accelerometer Range
-    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-    Serial.print("Accelerometer range set to: ");
+//     // set accelerometer Range
+//     mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+//     Serial.print("Accelerometer range set to: ");
 
-    switch (mpu.getAccelerometerRange())
-    {
-    case MPU6050_RANGE_2_G:
-        Serial.println("+-2G");
-        break;
-    case MPU6050_RANGE_4_G:
-        Serial.println("+-4G");
-        break;
-    case MPU6050_RANGE_8_G:
-        Serial.println("+-8G");
-        break;
-    case MPU6050_RANGE_16_G:
-        Serial.println("+-16G");
-        break;
-    }
+//     switch (mpu.getAccelerometerRange())
+//     {
+//     case MPU6050_RANGE_2_G:
+//         Serial.println("+-2G");
+//         break;
+//     case MPU6050_RANGE_4_G:
+//         Serial.println("+-4G");
+//         break;
+//     case MPU6050_RANGE_8_G:
+//         Serial.println("+-8G");
+//         break;
+//     case MPU6050_RANGE_16_G:
+//         Serial.println("+-16G");
+//         break;
+//     }
 
-    // set Gyro Range
-    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-    Serial.print("Gyro range set to: ");
-    switch (mpu.getGyroRange())
-    {
-    case MPU6050_RANGE_250_DEG:
-        Serial.println("+- 250 deg/s");
-        break;
-    case MPU6050_RANGE_500_DEG:
-        Serial.println("+- 500 deg/s");
-        break;
-    case MPU6050_RANGE_1000_DEG:
-        Serial.println("+- 1000 deg/s");
-        break;
-    case MPU6050_RANGE_2000_DEG:
-        Serial.println("+- 2000 deg/s");
-        break;
-    }
+//     // set Gyro Range
+//     mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+//     Serial.print("Gyro range set to: ");
+//     switch (mpu.getGyroRange())
+//     {
+//     case MPU6050_RANGE_250_DEG:
+//         Serial.println("+- 250 deg/s");
+//         break;
+//     case MPU6050_RANGE_500_DEG:
+//         Serial.println("+- 500 deg/s");
+//         break;
+//     case MPU6050_RANGE_1000_DEG:
+//         Serial.println("+- 1000 deg/s");
+//         break;
+//     case MPU6050_RANGE_2000_DEG:
+//         Serial.println("+- 2000 deg/s");
+//         break;
+//     }
 
-    // Set filter bandwidth
-    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-    Serial.print("Filter bandwidth set to: ");
-    switch (mpu.getFilterBandwidth())
-    {
-    case MPU6050_BAND_260_HZ:
-        Serial.println("260 Hz");
-        break;
-    case MPU6050_BAND_184_HZ:
-        Serial.println("184 Hz");
-        break;
-    case MPU6050_BAND_94_HZ:
-        Serial.println("94 Hz");
-        break;
-    case MPU6050_BAND_44_HZ:
-        Serial.println("44 Hz");
-        break;
-    case MPU6050_BAND_21_HZ:
-        Serial.println("21 Hz");
-        break;
-    case MPU6050_BAND_10_HZ:
-        Serial.println("10 Hz");
-        break;
-    case MPU6050_BAND_5_HZ:
-        Serial.println("5 Hz");
-        break;
-    }
-}
+//     // Set filter bandwidth
+//     mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+//     Serial.print("Filter bandwidth set to: ");
+//     switch (mpu.getFilterBandwidth())
+//     {
+//     case MPU6050_BAND_260_HZ:
+//         Serial.println("260 Hz");
+//         break;
+//     case MPU6050_BAND_184_HZ:
+//         Serial.println("184 Hz");
+//         break;
+//     case MPU6050_BAND_94_HZ:
+//         Serial.println("94 Hz");
+//         break;
+//     case MPU6050_BAND_44_HZ:
+//         Serial.println("44 Hz");
+//         break;
+//     case MPU6050_BAND_21_HZ:
+//         Serial.println("21 Hz");
+//         break;
+//     case MPU6050_BAND_10_HZ:
+//         Serial.println("10 Hz");
+//         break;
+//     case MPU6050_BAND_5_HZ:
+//         Serial.println("5 Hz");
+//         break;
+//     }
+// }
 
 void setup()
 {
-    Serial.begin(115200);                      // TypeC
-    Serial1.begin(115200, SERIAL_8N1, 20, 21); // RS232: rx = 20, tx = 21
-    delay(1000);
+    Serial.begin(BUAD_RATE_0);                                  // TypeC
+    Serial1.begin(BUAD_RATE_1, SERIAL_8N1, RX_1_PIN, TX_1_PIN); // RS232: rx = 16, tx = 17
+    // delay(1000);
     Serial.println("ESP32 Receiver Serial Ready!");
     Serial1.println("ESP32 Receiver Serial1 Ready!");
 
-    Wire.begin(2, 3, 400000L); // SDA, SCL, 400kHz
-    setup_mpu6050();
-    delay(100);
+    // Wire.begin(SDA_PIN, SCL_PIN, I2C_SPEED); // SDA = 13, SCL = 14, 400kHz
+    // setup_mpu6050();
+    // delay(100);
 
     // Set the RC receiver pins as inputs and attach the interrupts
     for (int i = 0; i < 4; i++)
@@ -375,7 +386,7 @@ void setup()
         attachInterrupt(digitalPinToInterrupt(Channels[i]), isr_functions[i], CHANGE);
     }
 
-    // 分配PWM输出通道到管脚
+    // // 分配PWM输出通道到管脚
     ledcAttachChannel(STEERING_PIN, 50, 14, CH_STEERING);
     ledcAttachChannel(THROTTLE_PIN, 50, 14, CH_THROTTLE);
 
@@ -390,18 +401,19 @@ void setup()
 
     // esp_now_register_recv_cb(OnDataRecv);
 
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-    FastLED.setBrightness(BRIGHTNESS);
+    // FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+    // FastLED.setBrightness(BRIGHTNESS);
 
     // 替换原有直接设置颜色的方式
-    setLEDColor(CRGB::Blue); // 使用新函数设置初始颜色
-    delay(1000);
+    // setLEDColor(CRGB::Blue); // 使用新函数设置初始颜色
+    // delay(1000);
 
     // SETUP ENDS
 }
 
 void loop()
 {
+    // Serial.println("ESP32 OK!");
     //  Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
     //   read_ina219();
     //   read_mpu6050();
@@ -451,7 +463,7 @@ void loop()
             car_output.throttle = 0;
             if (carOutputModeLast != CAR_MODE_FULL_AUTO || toggleActive == false)
             {
-                setLEDToggle(CRGB::Blue, CRGB::Red);
+                // setLEDToggle(CRGB::Blue, CRGB::Red);
                 carOutputModeLast = CAR_MODE_FULL_AUTO;
             }
             // if (!toggleActive)
@@ -461,7 +473,7 @@ void loop()
         }
         else
         {
-            setLEDColor(CRGB::Blue); // set LED to Red
+            // setLEDColor(CRGB::Blue); // set LED to Red
             car_output.throttle = pilot_data.throttle;
         }
         car_output.steering = pilot_data.steering;
@@ -474,13 +486,13 @@ void loop()
             car_output.throttle = 0;
             if (carOutputModeLast != CAR_MODE_SEMI_AUTO || toggleActive == false)
             {
-                setLEDToggle(CRGB::Yellow, CRGB::Red);
+                // setLEDToggle(CRGB::Yellow, CRGB::Red);
                 carOutputModeLast = CAR_MODE_SEMI_AUTO;
             }
         }
         else
         {
-            setLEDColor(CRGB::Yellow); // set LED to blue
+            // setLEDColor(CRGB::Yellow); // set LED to blue
             car_output.throttle = map(rc_data.throttle - 1493, 888 - 1493, 2149 - 1493, -100, 100);
         }
         car_output.steering = pilot_data.steering;
@@ -494,13 +506,13 @@ void loop()
             // setLEDColor(CRGB::Red); // set LED to blue
             if (carOutputModeLast != CAR_MODE_MANUAL || toggleActive == false)
             {
-                setLEDToggle(CRGB::Green, CRGB::Red);
+                // setLEDToggle(CRGB::Green, CRGB::Red);
                 carOutputModeLast = CAR_MODE_MANUAL;
             }
         }
         else
         {
-            setLEDColor(CRGB::Green); // set LED to blue
+            // setLEDColor(CRGB::Green); // set LED to blue
 
             // RC => CAR
             car_output.throttle = map(rc_data.throttle - 1493, 888 - 1493, 2149 - 1493, -100, 100);
@@ -543,9 +555,11 @@ void loop()
 
     ledcWriteChannel(CH_STEERING, pwm_steering);
     ledcWriteChannel(CH_THROTTLE, pwm_throttle);
+    // ledcWrite(STEERING_PIN, pwm_steering);
+    // ledcWrite(THROTTLE_PIN, pwm_throttle);
 
     delay(10);
     counter += 1;
 
-    scanLEDToggle();
+    // scanLEDToggle();
 }

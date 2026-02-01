8473716aa5eb61324b28599eb65e7298e54901da
Nowhere_X <haobot@foxmail.com>
2026-02-02 00:23:33 +0800

    fix(ble): 修正游戏手柄摇杆映射和通道分配
    调整左右摇杆的映射范围，将转向通道映射到右摇杆X轴（0-32767），
    油门通道映射到左摇杆Y轴（32767-0）。同时限制油门通道的有效输入范围为1300-1800，
    以匹配实际遥控器的死区设置。

--- mus4/mus4.ino
+++ mus4/mus4.ino
@@ -505,15 +505,15 @@ void read_ina219()
 void sendGamepadPacket() {
     if (bleGamepad.isConnected()) {
         // Map RC Channels (approx 1000-2000) to Gamepad Axes (-32767 to 32767)
-        int lx = map(constrain(pwm_value[CH_STEERING], 1000, 2000), 1000, 2000, -32767, 32767);
-        int ly = map(constrain(pwm_value[CH_THROTTLE], 1000, 2000), 1000, 2000, -32767, 32767);
+        int lx = map(constrain(pwm_value[CH_STEERING], 1000, 2000), 1000, 2000, 0, 32767);
+        int ly = map(constrain(pwm_value[CH_THROTTLE], 1300, 1800), 1300, 1800, 32767, 0);
         // int rx = map(constrain(pwm_value[CH_PARK], 1000, 2000), 1000, 2000, -32767, 32767);
         // int ry = map(constrain(pwm_value[CH_MODE], 1000, 2000), 1000, 2000, -32767, 32767);
         int rx = 0;
         int ry = 0;
 
-        bleGamepad.setLeftThumb(lx, ly);
-        bleGamepad.setRightThumb(0, 0);
+        bleGamepad.setLeftThumb(0, ly);
+        bleGamepad.setRightThumb(lx, 0);
     }
 }
 #endif
